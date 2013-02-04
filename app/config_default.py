b169e472ebd4031b6d553b053f46a85462e5814b
Matt Hughes <hughesm02@gmail.com>
2013-02-04 09:24:37 -0500

    ignore config.py

---
+++ app/config_default.py
@@ -0,0 +1,11 @@
+# DB configuration
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'stats',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': ''
+    }
+}
